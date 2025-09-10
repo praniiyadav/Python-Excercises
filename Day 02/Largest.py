db908029c656f9c0fe1a0400138c68e4befb3c6d
Praneeth Mopuri <mr.prani16@gmail.com>
2025-09-10 15:54:17 +0530

    Update Largest.py

--- Day 02/Largest.py
+++ Day 02/Largest.py
@@ -8,4 +8,13 @@ if a>=b and a>=c:
 elif b>=a and b>=c:
     print("b is the largest number")
 else:
-    print("s is the largest number")
+    print("c is the largest number")
+
+
+'''
+Output:
+enter first number:16
+enter second number:03
+enter third number:15
+a is the largest number
+'''
