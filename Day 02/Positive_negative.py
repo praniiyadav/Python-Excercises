6dddd04cc948b1e14794968a37b034605d0b3081
Praneeth Mopuri <mr.prani16@gmail.com>
2025-09-10 15:53:56 +0530

    Update Positive_negative.py

--- Day 02/Positive_negative.py
+++ Day 02/Positive_negative.py
@@ -6,3 +6,20 @@ elif number < 0:
     print("the number is negative.")
 else:
     print("the number is zero")
+
+
+'''
+output:
+#positvie 
+Enter a number:9
+the number is positive.
+#negative
+Enter a number:-5
+the number is negative.
+#Zero
+Enter a number:0
+the number is zero
+
+
+
+'''
