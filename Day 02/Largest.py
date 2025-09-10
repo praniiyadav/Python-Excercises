d14160b15781eb3efccc5c51859098509a7f8e57
Praneeth Mopuri <mr.prani16@gmail.com>
2025-09-10 15:52:31 +0530

    Add files via upload

--- Day 02/Largest.py
+++ Day 02/Largest.py
@@ -1,1 +1,11 @@
 
+a = float(input("enter first number:")) 
+b = float(input("enter second number:"))
+c = float(input("enter third number:"))
+
+if a>=b and a>=c:
+    print("a is the largest number")
+elif b>=a and b>=c:
+    print("b is the largest number")
+else:
+    print("s is the largest number")
