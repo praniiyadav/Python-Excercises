8a4459c7631493cc79871150329ab5f097584cea
Praneeth Mopuri <mr.prani16@gmail.com>
2025-09-11 11:03:36 +0530

    Add files via upload

---
+++ Day 03/Multiple.py
@@ -0,0 +1,23 @@
+num = int(input("enter a numner:"))
+
+print("the mutliple of number is:")
+
+for i in range(1,11):
+    print(f"{num} x {i}  = {num * i} ")
+
+
+'''
+Output:
+589 x 1  = 589 
+589 x 2  = 1178
+589 x 3  = 1767
+589 x 4  = 2356
+589 x 5  = 2945
+589 x 6  = 3534
+589 x 7  = 4123
+589 x 8  = 4712
+589 x 9  = 5301
+589 x 10  = 5890
+
+
+'''
--- Day 03/sum.py
+++ Day 03/sum.py
@@ -1,1 +1,14 @@
+num = int(input("enter a number:"))
 
+total = 0
+
+for i in range (1, num +1):
+    total += i #total = total + i
+print("the sum of number from 1 to ",  num, "is:", total )
+
+
+'''
+Output:
+enter a number:16
+the sum of number from 1 to  16 is: 136
+'''
